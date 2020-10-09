6ccf587cc60e1e840fa2d2558ad685716d3f7a7c
Jennifer Kramer <jenkrame@gmail.com>
2020-10-09 17:38:04 -0400

    Fixed styles on Character.js file.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import axios from 'axios';
 import Character from './components/Character.js';
 import styled from 'styled-components';
+// import GlobalFonts from './fonts/Fonts.js'
 
-const FlexDiv = styled.div`
+const CharDiv = styled.div`
     width: 100%;
     max-width: 100%;
     padding: 5%;
@@ -18,18 +19,15 @@ const App = () => {
   // the state properties here.
 
   const [characterData, setCharacterData] = useState([]);
-  const [currentCharacter, setCurrentCharacter] = useState(null);
-
 
   useEffect(() => {
-    axios.get('https://swapi.dev/api/people/')
-      .then(res => {
-        setCharacterData(res.data.results)
-        // console.log(res.data.results)
-      })
-      .catch(err => {
-        console.log(err)
-      })
+    axios.get(`https://swapi.dev/api/people/`)
+    .then(res => {
+      setCharacterData(res.data.results)
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }, []);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
@@ -41,9 +39,10 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <FlexDiv className='Body'>
+      <CharDiv className='Body'>
+        {/* <GlobalFonts /> */}
         {characterData.map(v => <Character data={v} key={charsKey++}></Character>)}
-      </FlexDiv>
+      </CharDiv>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -10,7 +10,7 @@ const CharBox = styled.div`
     margin: 2%;
     border-radius: 10px;
     width: 20%;
-    background: rgba(255,255,255,.7)
+    background: rgba(255,255,255,.7);
 `
 
 const Character = (props) => {
@@ -18,20 +18,24 @@ const Character = (props) => {
     let charBox;
 
     useEffect(() => {
-        gsap.from(charBox, {y: -50, delay: props.key*50})
+        gsap.from(charBox, {y: -50})
     }, [])
 
     return (
         <CharBox ref={div => charBox = div}>
             <h2>{props.data.name}</h2>
             <div>
-                <p>Gender: {props.data.gender}</p>
+                <p>Gender: {
+                    props.data.gender === 
+                    "n/a" ? "N/A"
+                    : props.data.gender.charAt(0).toUpperCase() + props.data.gender.slice(1)
+                }</p>
                 <p>Height: {props.data.height}</p>
                 <p>Mass: {props.data.mass}</p>
                 <p>Birth Year: {props.data.birth_year}</p>
-                <p>Eye Color: {props.data.eye_color}</p>
-                <p>Hair Color: {props.data.hair_color}</p>
-                <p>Skin Color: {props.data.skin_color}</p>
+                <p>Eye Color: {props.data.eye_color.charAt(0).toUpperCase() + props.data.eye_color.slice(1)}</p>
+                <p>Hair Color: {props.data.hair_color.charAt(0).toUpperCase() + props.data.hair_color.slice(1)}</p>
+                <p>Skin Color: {props.data.skin_color.charAt(0).toUpperCase() + props.data.skin_color.slice(1)}</p>
             </div>
         </CharBox>
     )
